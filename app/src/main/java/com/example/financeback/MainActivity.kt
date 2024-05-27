2a1e648eea91f31d6d15070747039c0a17bfdc50
Joao Heringer <jounheringer@outlook.com>
2024-05-26 22:08:18 -0400

    Tela de configuração, ajuda e restaurar

--- app/src/main/java/com/example/financeback/MainActivity.kt
+++ app/src/main/java/com/example/financeback/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -40,12 +42,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavType
@@ -61,6 +61,7 @@ import com.example.financeback.classes.User
 import com.example.financeback.classes.UserInfo
 import com.example.financeback.screens.LoginScreen
 import com.example.financeback.screens.Screen
+import com.example.financeback.screens.compose.ConfigurationsScreen
 import com.example.financeback.screens.compose.EditScreen
 import com.example.financeback.screens.compose.HomeScreen
 import com.example.financeback.screens.compose.IncomeScreen
@@ -89,18 +90,42 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun FinanceBackScreen(modifier: Modifier = Modifier, userID: Int, context: Context) {
     val user = User()
-    var userInfo by remember { mutableStateOf(UserInfo()) }
-    var updatedUser by remember { mutableStateOf(true) }
     val navController = rememberNavController()
     val scope = rememberCoroutineScope()
     val items = listOf(Screen.Home, Screen.Income, Screen.Report)
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
+    var userInfo by remember { mutableStateOf(UserInfo()) }
+    var updatedUser by remember { mutableStateOf(true) }
+    var restartClicked by remember { mutableStateOf(false) }
     var screenTitle by remember { mutableStateOf(Screen.Home.title) }
+    var helpClicked by remember { mutableStateOf(false) }
 
     if (updatedUser){
         userInfo = user.getUserByID(context, userID)
         updatedUser = false
+    }
+    
+    if(helpClicked) {
+        AlertDialog(onDismissRequest = { helpClicked = false },
+            text = { Text(text = "Deseja baixar o guia de utilização do aplicativo?")},
+            confirmButton = { Button(onClick = { helpClicked = false }) {
+                Text(text = "Baixar")
+            } },
+            dismissButton = { Button(onClick = { helpClicked = false }) {
+                Text(text = "Cancelar")
+            } })
+    }
+
+    if(restartClicked) {
+        AlertDialog(onDismissRequest = { restartClicked = false }, 
+            text = { Text(text = "Ao restaurar o aplicativo sera reiniciado todos os usuarios e dados do aplicativo")},
+            confirmButton = { Button(onClick = { restartClicked = false }) {
+                Text(text = "Restaurar")
+            } },
+            dismissButton = { Button(onClick = { restartClicked = false }) {
+                Text(text = "Cancelar")
+            } })
     }
 
     ModalNavigationDrawer(drawerState = drawerState,
@@ -143,13 +168,9 @@ fun FinanceBackScreen(modifier: Modifier = Modifier, userID: Int, context: Conte
                         contentDescription = Screen.Help.description,
                         icon = Screen.Help.icon,
                         sensitiveItem = false,
-                        onClick = {navController.navigate(Screen.Help.route) {
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                }
-                        scope.launch{drawerState.close() }
-                            screenTitle = Screen.Help.title}
+                        onClick = {helpClicked = true
+                                scope.launch{drawerState.close() }
+                        }
                     ),
                     MenuItem(
                         id = Screen.LogOut.route,
@@ -165,13 +186,9 @@ fun FinanceBackScreen(modifier: Modifier = Modifier, userID: Int, context: Conte
                         contentDescription = Screen.Restart.description,
                         icon = Screen.Restart.icon,
                         sensitiveItem = true,
-                        onClick = {navController.navigate(Screen.Restart.route) {
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                }
-                        scope.launch{drawerState.close() }
-                        screenTitle = Screen.Restart.title}
+                        onClick = {restartClicked = true
+                                scope.launch{drawerState.close() }
+                        }
                     )
                 ))
         } })
@@ -262,27 +279,8 @@ fun FinanceBackScreen(modifier: Modifier = Modifier, userID: Int, context: Conte
                             updatedUser = { updatedUser = it })
                     }
                     composable(Screen.Settings.route) {
-                        Toast.makeText(
-                            context,
-                            Screen.Settings.route,
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        ConfigurationsScreen()
                     }
-                    composable(Screen.Help.route) {
-                        Toast.makeText(
-                            context,
-                            Screen.Help.route,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    composable(Screen.Restart.route) {
-                        Toast.makeText(
-                            context,
-                            Screen.Restart.route,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
                 }
             }
         }
---
+++ app/src/main/java/com/example/financeback/screens/compose/ConfigurationsCompose.kt
@@ -0,0 +1,95 @@
+package com.example.financeback.screens.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ConfigurationsScreen(modifier: Modifier = Modifier) {
+    var fontSize by remember{ mutableIntStateOf(12) }
+    var darkMode by remember{ mutableStateOf(false) }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(300.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Modo escuro")
+                Spacer(modifier = modifier.width(10.dp))
+                Switch(checked = darkMode, onCheckedChange = { darkMode = !darkMode })
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Tamanho da fonte")
+                Spacer(modifier = modifier.width(10.dp))
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    IconButton(onClick = { fontSize++ },
+                        enabled = fontSize < 32) {
+                        Icon(imageVector = Icons.Filled.KeyboardArrowUp, contentDescription = null)
+                    }
+                    OutlinedCard(modifier = modifier.width(IntrinsicSize.Max)) {
+                        Column(
+                            modifier = modifier
+                                .widthIn(48.dp, 128.dp)
+                                .height(48.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = fontSize.toString(),
+                                fontSize = 18.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    IconButton(onClick = { fontSize-- },
+                        enabled = fontSize > 10) {
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowDown,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConfigurationScreen() {
+    ConfigurationsScreen()
+}
